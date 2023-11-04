f7612742abb1f872d983dc85dc47654840db0d73
Analox <114465675+TomShir@users.noreply.github.com>
2023-11-04 21:15:37 +0000

    Update ascii.py

--- ascii.py
+++ ascii.py
@@ -1,123 +1,119 @@
-import random 
+#create a calculator 
 from colorama import Fore 
 import time 
 import sys 
+import random
+
 run_condition='True'
-title='rock paper scissors'
-color_txt=Fore.RED,Fore.GREEN,Fore.BLUE
-for n in title:
-  sys.stdout.flush()
-  time.sleep(0.2)
-  sys.stdout.write(f'\t{random.choice(color_txt)}{n.upper()}')
+class Math_functions:
+    def __init__(self,flnumerical,flinteger):
+        self.flnumerical=flnumerical 
+        self.flinteger=flinteger 
+        
+    def addition(self):
+        ans= lambda argv:argv + self.flnumerical
+        print(ans(self.flinteger))
+        
+    def subtraction(self):
+        ans= lambda argv:argv - self.flinteger 
+        print(ans(self.flnumerical))
+        
+    def multiplication(self):
+        ans= lambda argv:argv * self.flnumerical 
+        print(ans(self.flinteger))
+        
+    def division(self):
+        ans= lambda argv:argv / self.flinteger 
+        print(ans(self.flnumerical))
+        
+    def remainder(self):
+        ans= lambda argv:argv % self.flinteger
+        print(ans(self.flnumerical))
+        
+title='calculator\n'
+txt_color=Fore.RED,Fore.GREEN,Fore.BLUE,Fore.YELLOW
+def flnumerical_input():
+        global flnumerical 
+        flnumerical=float(input('Decimal or whole number:'))
+def flinteger_input():
+        global flinteger 
+        flinteger=float(input('Decimal or whole number:'))
+        
+        
+def error_msg(txt):
+    for x in txt:
+     time.sleep(0.2)
+     print(Fore.RED+x,flush=False)
+    else:
+      global reset_to_default_color 
+      reset_to_default_color=Fore.RESET 
+      print(f'{reset_to_default_color}')
+    if txt=='':
+      pass  
+
+error_msg(txt='')
+    
+for txt in title:
+    sys.stdout.flush()
+    time.sleep(0.2)
+    sys.stdout.write(f'\t {random.choice(txt_color)}{txt.upper()}')
 else:
-  reset_to_default_color=Fore.RESET 
-  print(f'{reset_to_default_color}')
-  time.sleep(0.2)
-  print(f'{1}:1 player,\n2:2 players\n')
-  while bool(run_condition):
-     play=str(input('1 player or 2 players? or q to exit program:'))
-     if play=='1':
-      time.sleep(0.2)
-      rock='rock'
-      paper='paper'
-      scissors='scissors'
-      options=['rock','paper','scissors']
-      for n in options:
-          if n==options[0]:
-              print(f'{options[0][0]}:{n}')
-          elif n==options[1]:
-              print(f'{options[1][0]}:{n}')
-          elif n==options[-1]:
-              print(f'{options[-1][0]}:{n}')
-      else:
-          option_input=input('r,p or s:')
-          time.sleep(0.2)
-          computer_options=[options[0][0],options[1][0],options[-1][0]]
-          random_option=random.choice(computer_options)
-          computer_won_msg='The computer won!'
-          player_won_msg='The player won!'
-          tie_msg='Its a tie!'
-          print('Computer generating random option.....')
-          time.sleep(0.2)
-          print(random_option)
-          time.sleep(0.2)
-          def win_condition():
-            if play=='1':
-             if random_option==computer_options[0]:
-                print(f'The computer chose {rock}')
-             elif random_option==computer_options[1]:
-                print(f'The computer chose {paper}')
-             elif random_option==computer_options[-1]:
-                print(f'The computer chose {scissors}')
-             if option_input==computer_options[0] and random_option==computer_options[0]:
-              print(tie_msg)
-             elif option_input==computer_options[0] and random_option==computer_options[1]:
-              print(computer_won_msg)
-             elif option_input==computer_options[0] and random_option==computer_options[-1]:
-              print(player_won_msg)
-             elif option_input==computer_options[1] and random_option==computer_options[0]:
-              print(player_won_msg)
-             elif option_input==computer_options[1] and random_option==computer_options[1]:
-              print(tie_msg)
-             elif option_input==computer_options[1] and random_option==computer_options[-1]:
-              print(computer_won_msg)
-             elif option_input==computer_options[-1] and random_option==computer_options[-1]:
-              print(tie_msg)
-             elif option_input==computer_options[-1] and random_option==computer_options[0]:
-              print(computer_won_msg)
-             elif option_input==computer_options[-1] and random_option==computer_options[1]:
-              print(computer_won_msg)
-              
-      win_condition()
-            #if user wants to exit program
-     elif play=='q':
-         break
-     elif play=='2':
-                 your_turn='You"re going first'
-                 second_player_turn='Now it"s the second player"s turn'
-                 print(your_turn)
-                 time.sleep(0.2)
-                 options=['rock','paper','scissors']
-                 moves=[options[0][0],options[1][0],options[-1][0]]
-                 def choice_input():
-                     for n in moves:
-                         if n==moves[0]:
-                             print(f'rock:{n}')
-                         elif n==moves[1]:
-                             print(f'paper:{n}')
-                         elif n==moves[2]:
-                             print(f'scissors:{n}')
-                     else:
-                         global input_move_player_1
-                         input_move_player_1=str(input('r or p or s:'))
-                         if input_move_player_1:
-                             global input_move_player_2 
-                             print(second_player_turn)
-                             time.sleep(0.2)
-                             input_move_player_2=str(input('r or p or s:'))
-                             
-                 choice_input()
-                 def win_condition_1():
-                     player_one_won='player 1 won!'
-                     player_two_won='player 2 won!'
-                     tie_msg='It"s a tie'
-                     if input_move_player_1==moves[0] and input_move_player_2==moves[0]:
-                         print(f'Since you and the second player both chose {input_move_player_1} {tie_msg}')
-                     elif input_move_player_1==moves[1] and input_move_player_2==moves[1]:
-                         print(f'Since you and the second player both chose  {input_move_player_1} {tie_msg}')
-                     elif input_move_player_1==moves[2] and input_move_player_2==moves[2]:
-                         print(f'Since you and the second player both chose {input_move_player_1} {tie_msg}')
-                     elif input_move_player_1==moves[0] and input_move_player_2==moves[1]:
-                         print(f'Since paper beats rock  {player_two_won}')
-                     elif input_move_player_1==moves[0] and input_move_player_2==moves[2]:
-                         print(f'Since rock beats scissors{player_one_won}')
-                     elif input_move_player_1==moves[1] and input_move_player_2==moves[2]:
-                         print(f'Since scissors beats paper {player_two_won}')
-                     elif input_move_player_1==moves[1] and input_move_player_2==moves[0]:
-                         print(f'Since paper beats rock {player_one_won}')
-                     elif input_move_player_1==moves[2] and input_move_player_2==moves[1]:
-                         print(f'Since scissors beats paper {player_one_won}')
-                     elif input_move_player_1==moves[2] and input_move_player_2==moves[0]:
-                         print(f'Since rock beats scissors {player_two_won}')
-                       
-                 win_condition_1()
+    print(f'{reset_to_default_color}')
+    while bool(run_condition):
+        try:
+            flnumerical_input()
+            time.sleep(0.2)
+            print(flnumerical)
+            time.sleep(0.2)
+            arithmetic_operator=str(input('arithmetic_operator:'))
+            #operators:
+            operator_1='+'
+            operator_2='-'
+            operator_3='*'
+            operator_4='/'
+            operator_5='%'
+            equal_sign='='
+            if arithmetic_operator==operator_1:
+                print(f'{flnumerical} {operator_1}')
+                time.sleep(0.2)
+                flinteger_input()
+                time.sleep(0.2)
+                print(f'{flnumerical} {operator_1} {flinteger} {equal_sign}')
+                math_obj=Math_functions(flnumerical=flnumerical,flinteger=flinteger)
+                math_obj.addition()
+            elif arithmetic_operator==operator_2:
+                print(f'{flnumerical} {operator_2}')
+                time.sleep(0.2)
+                flinteger_input()
+                time.sleep(0.2)
+                print(f'{flnumerical} {operator_2} {flinteger} {equal_sign}')
+                math_obj=Math_functions(flnumerical=flnumerical,flinteger=flinteger)
+                math_obj.subtraction()
+            elif arithmetic_operator==operator_3:
+                print(f'{flnumerical} {operator_3}')
+                time.sleep(0.2)
+                flinteger_input()
+                time.sleep(0.2)
+                print(f'{flnumerical} {operator_3} {flinteger} {equal_sign}')
+                math_obj=Math_functions(flnumerical=flnumerical,flinteger=flinteger)
+                math_obj.multiplication()
+            elif arithmetic_operator==operator_4:
+                print(f'{flnumerical} {operator_4}')
+                time.sleep(0.2)
+                flinteger_input()
+                time.sleep(0.2)
+                print(f'{flnumerical} {operator_4} {flinteger} {equal_sign}')
+                math_obj=Math_functions(flnumerical=flnumerical,flinteger=flinteger)
+                math_obj.division()
+            elif arithmetic_operator==operator_5:
+                print(f'{flnumerical} {operator_5}')
+                time.sleep(0.2)
+                flinteger_input()
+                time.sleep(0.2)
+                print(f'{flnumerical} {operator_5} {flinteger} {equal_sign}')
+                math_obj=Math_functions(flnumerical=flnumerical,flinteger=flinteger)
+                math_obj.remainder()
+            elif arithmetic_operator!=(operator_1 or operator_2 or operator_3 or operator_4 or operator_5 ):
+                error_msg(txt='Invalid operator')
+        except ValueError:
+            error_msg(txt='Invalid value')
